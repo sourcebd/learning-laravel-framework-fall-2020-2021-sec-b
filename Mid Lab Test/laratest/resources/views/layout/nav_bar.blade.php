f5d9ded20bde4095d1c142f5d4aea5ffd8e3a9b2
sourcebd <nativetube71@gmail.com>
2021-03-02 01:06:53 +0600

    Some Changes

--- Mid Lab Test/laratest/resources/views/layout/nav_bar.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.main')
-
-
-@section('nav_bar')
-<a href="/home/create">Create user</a> |
-<a href="{{route('home.userlist')}}">View user list</a> |
-<a href="/logout">logout</a>
-@endsection
